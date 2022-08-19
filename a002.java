cde8e31bdb1b591d1d0ce983ff9dc6b5b7a3ee48
AmanoShizukikun <hackamsk@gmail.com>
2022-08-19 11:21:44 +0800

    ZeroJudge 2022/8/19

---
+++ a002.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class a002 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+        System.out.print(number1 + number2);
+    }
+}
---
+++ a002.py
@@ -0,0 +1,2 @@
+number1 , number2= map(int,input().split())
+print(number1 + number2)
