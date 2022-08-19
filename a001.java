c514bbacadd57db582b66bad3d590012ca365a21
AmanoShizukikun <hackamsk@gmail.com>
2022-08-19 10:44:09 +0800

    ZeroJudge 2022/8/19

---
+++ a001.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class a001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.print("hello, " + word);
+    }
+}
---
+++ a001.py
@@ -0,0 +1,2 @@
+word = input()
+print("hello, " + word)
